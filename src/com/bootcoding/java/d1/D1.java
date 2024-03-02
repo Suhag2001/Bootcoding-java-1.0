53f9cde12b0ec74d3be51bbd9f5d5651eecd50e4
= <=>
2024-03-02 20:34:17 +0530

    D1 and Example1 added

--- src/com/bootcoding/java/d1/D1.java
+++ src/com/bootcoding/java/d1/D1.java
@@ -3,5 +3,9 @@ package com.bootcoding.java.d1;
 public class D1 {
     public static void main(String[] args) {
         System.out.println("we are from D1 fethod");
+
+
+        System.out.println("we are adding file by intellij idea");
+
     }
 }
---
+++ src/com/bootcoding/java/string/Example1.java
@@ -0,0 +1,63 @@
+package com.bootcoding.java.string;
+
+
+public class Example1 {
+    public static void main(String[] args) {
+
+//        String name = "Tanu";  // "Tanu"
+//
+//        String name2 = name;    // "Tanu"
+//
+//        name = "Ajay";
+//
+//        name = "Komal";
+//
+//
+//        System.out.println(name);
+//        System.out.println(name2);
+
+//
+//        String n1 = "Boot";
+//        String n2 = "boot";
+//
+//        String value = n1.concat(n2);
+//        String value2 = n1 + n2 ;
+//
+//        System.out.println(value);
+//        System.out.println(value2);
+//
+//        System.out.println(value.length());
+//        System.out.println(n1.length());
+//        System.out.println(n2.length());
+//
+//        System.out.println(value.toUpperCase());
+//        System.out.println(value.toLowerCase());
+//
+//        boolean res = n1.equals(n2);
+//        System.out.println(res);
+
+//        String name = "Bhavika";
+//
+//        char res = name.charAt(name.length()-1);
+//
+//        System.out.println(res);
+
+//        String fruit = "Apple Banana Tomato Waternut";
+//
+//       String[] res = fruit.split(" ");
+//
+//       for (int i =0 ; i< res.length ; i++) {
+//           System.out.println(res[i]);
+//       }
+
+       String  str =  "        Bhavika";
+
+
+        System.out.println(str);
+        System.out.println(str.trim());
+
+
+
+
+    }
+}
