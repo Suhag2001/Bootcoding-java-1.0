e699f39ba38b2704089c5d3e1c2a2851f8f80bf1
= <=>
2024-03-02 20:41:46 +0530

    D1 added

--- src/com/bootcoding/java/d1/D1.java
+++ src/com/bootcoding/java/d1/D1.java
@@ -3,5 +3,8 @@ package com.bootcoding.java.d1;
 public class D1 {
     public static void main(String[] args) {
         System.out.println("we are from D1 fethod");
+
+
+        System.out.println("this add from intllij idea");
     }
 }
